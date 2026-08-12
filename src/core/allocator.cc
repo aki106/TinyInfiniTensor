44843ba8a2837929a6f2c594e8c05b1b90175660
aki106 <yuxins.aki@gmail.com>
2026-08-12 23:08:07 +0800

    Implement allocator allocation and recycling

--- src/core/allocator.cc
+++ src/core/allocator.cc
@@ -29,17 +29,66 @@ namespace infini
         // pad the size to the multiple of alignment
         size = this->getAlignedSize(size);
 
+        IT_ASSERT(size > 0);
+        auto best = freeBlocks.end();
+        for (auto it = freeBlocks.begin(); it != freeBlocks.end(); ++it)
+            if (it->second >= size &&
+                (best == freeBlocks.end() || it->second < best->second))
+                best = it;
+
+        if (best != freeBlocks.end())
+        {
+            const auto addr = best->first;
+            const auto blockSize = best->second;
+            freeBlocks.erase(best);
+            if (blockSize > size)
+                freeBlocks.emplace(addr + size, blockSize - size);
+            return addr;
+        }
+
+        const auto addr = used;
+        used += size;
+        peak = std::max(peak, used);
+
         // =================================== 作业 ===================================
         // TODO: 设计一个算法来分配内存，返回起始地址偏移量
         // =================================== 作业 ===================================
 
-        return 0;
+        return addr;
     }
 
     void Allocator::free(size_t addr, size_t size)
     {
         IT_ASSERT(this->ptr == nullptr);
         size = getAlignedSize(size);
+
+        IT_ASSERT(size > 0);
+        IT_ASSERT(addr % alignment == 0 && addr + size <= used);
+
+        auto next = freeBlocks.lower_bound(addr);
+        if (next != freeBlocks.end() && addr + size == next->first)
+        {
+            size += next->second;
+            freeBlocks.erase(next);
+        }
+
+        auto right = freeBlocks.lower_bound(addr);
+        if (right != freeBlocks.begin())
+        {
+            auto prev = std::prev(right);
+            IT_ASSERT(prev->first + prev->second <= addr);
+            if (prev->first + prev->second == addr)
+            {
+                addr = prev->first;
+                size += prev->second;
+                freeBlocks.erase(prev);
+            }
+        }
+
+        if (addr + size == used)
+            used = addr;
+        else
+            freeBlocks.emplace(addr, size);
 
         // =================================== 作业 ===================================
         // TODO: 设计一个算法来回收内存
@@ -58,6 +107,8 @@ namespace infini
 
     size_t Allocator::getAlignedSize(size_t size)
     {
+        if (size == 0)
+            return 0;
         return ((size - 1) / this->alignment + 1) * this->alignment;
     }
 
