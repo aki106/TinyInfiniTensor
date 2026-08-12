e17f27cd9ffe3a5e50b2a6ba4caeec895bbcffbe
aki106 <yuxins.aki@gmail.com>
2026-08-12 23:14:25 +0800

    Implement concat shape inference

--- src/operators/concat.cc
+++ src/operators/concat.cc
@@ -4,6 +4,7 @@
 namespace infini {
 ConcatObj::ConcatObj(GraphObj *graph, TensorVec inputs, Tensor output, int _dim)
     : OperatorObj(OpType::Concat, inputs, {output}) {
+    IT_ASSERT(!inputs.empty());
     int rank = inputs[0]->getRank();
     dim = get_real_axis(_dim, rank);
     IT_ASSERT(checkValid(graph));
@@ -12,6 +13,16 @@ ConcatObj::ConcatObj(GraphObj *graph, TensorVec inputs, Tensor output, int _dim)
 optional<vector<Shape>> ConcatObj::inferShape(const TensorVec &inputs) {
     Shape dims = inputs[0]->getDims();
     auto rank = inputs[0]->getRank();
+
+    IT_ASSERT(dim >= 0 && dim < static_cast<int>(rank));
+    dims[dim] = 0;
+    for (const auto &input : inputs) {
+        const auto inputDims = input->getDims();
+        IT_ASSERT(inputDims.size() == rank);
+        for (size_t i = 0; i < rank; ++i)
+            IT_ASSERT(i == static_cast<size_t>(dim) || inputDims[i] == dims[i]);
+        dims[dim] += inputDims[dim];
+    }
 
     // =================================== 作业 ===================================
     // TODO：修改 dims，返回正确的 concat 后的 shape
